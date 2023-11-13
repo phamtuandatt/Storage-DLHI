9d4e98f2ebb7fc9c66c991668042342570cfb834
phamtuandat16072001@gmail.com <phamtuandat16072001@gmail.com>
2023-11-13 17:18:48 +0700

    Add GUI Import

---
+++ Storage/Storage/GUI_Import/ucImport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Storage.GUI_Import
+{
+    public partial class ucImport : UserControl
+    {
+        public ucImport()
+        {
+            InitializeComponent();
+        }
+    }
+}
