5773980888b8b95ecd95e8682782171d78c0c739
phamtuandat16072001@gmail.com <phamtuandat16072001@gmail.com>
2023-11-13 15:44:13 +0700

    Add Validtion Helper class

---
+++ Storage/Storage/Helper/Validation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Helper
+{
+    internal class Validation
+    {
+        public const string NOTALLOWED = @"><@{}[]#&()/|*-+$%~\";
+    }
+}
