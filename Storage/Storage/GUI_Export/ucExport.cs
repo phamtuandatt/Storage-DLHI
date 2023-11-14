43bde26c3783463c1b6ab200ab96525028c57333
phamtuandat16072001@gmail.com <phamtuandat16072001@gmail.com>
2023-11-14 13:37:57 +0700

    Add GUI Export Item

---
+++ Storage/Storage/GUI_Export/ucExport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Storage.GUI_Export
+{
+    public partial class ucExport : UserControl
+    {
+        public ucExport()
+        {
+            InitializeComponent();
+        }
+    }
+}
