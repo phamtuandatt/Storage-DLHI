440269d19ed0719c3440e36b1bd1ea7c206f734a
phamtuandatt <109933954+phamtuandatt@users.noreply.github.com>
2023-11-20 15:57:01 +0700

    Add Dtos for Storage WebAPI

---
+++ WebAPI/WebAPI/WebAPI.BLL/DTOs/WareHouseDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.DTOs
+{
+    internal class WareHouseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int TotalItem { get; set; }
+    }
+
+    internal class WareHouse_DetailDto
+    {
+        public Guid WarehouseId { get; set; }
+        public Guid Item_Id { get; set; }
+        public int Quantity { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
