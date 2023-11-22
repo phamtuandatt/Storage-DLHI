51d2e297e7968d4fd646ca84f84466d3ff51a034
phamtuandatt <109933954+phamtuandatt@users.noreply.github.com>
2023-11-22 15:43:37 +0700

    Add response dto

---
+++ Storage/Storage/Response/ItemResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Response
+{
+    internal class ItemResponse
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string PICTURE_LINK { get; set; }
+        public string PICTURE { get; set; }
+        public string Unit { get; set; }
+        public string GROUPS { get; set; }
+        public string Supplier { get; set; }
+        public string Note { get; set; }
+        public string Eng_Name { get; set; }
+    }
+}
